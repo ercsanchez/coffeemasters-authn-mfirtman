134eea38ccc448936529c5c24ed0b63199e5b58b
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-03-22 07:58:53 +0800

    bugfix: set target for proxy to localhost's default port of 80 if NODE_ENV=production (for npm run preview) and port=5000 if NODE_ENV=development

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// vite build uses NODE_ENV=production and vite preview uses build files
+// console.log('NODE_ENV', process.env.NODE_ENV);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,7 +11,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: `http://localhost:${
+          process.env.NODE_ENV === 'production' ? 80 : 5000
+        }`,
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''),
       },
