cc05c01985191e7154257e572b5e7fa301b77a17
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-03-22 01:18:36 +0800

    test api route from client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <a href="/api/test">Test API route</a>
     </div>
   );
 }
