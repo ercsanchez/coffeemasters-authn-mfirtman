6828d171a179e71bf477c113307e6402090da759
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-03-25 23:43:16 +0800

    add /users (get all users) to test api and postgres/prisma connection

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,13 @@
 import { Router } from 'express';
 
+import prisma from '../services/db.js';
+
 const router = Router();
+
+router.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json({ data: users });
+});
 
 router.get('/test', (req, res) => {
   res.send('<h1>server response</h1>');
