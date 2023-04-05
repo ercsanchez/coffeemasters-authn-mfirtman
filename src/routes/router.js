c0defabb2ca18aa6b465795a55a23e7be6cf26e8
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-04-06 07:52:55 +0800

    3-10 create register route (/auth/register)

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,6 +4,19 @@ import prisma from '../services/db.js';
 
 const router = Router();
 
+router.post('/auth/register', async (req, res) => {
+  // TODO: Data Validation
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  const userFound = await prisma.user.findUniqueOrThrow({
+    where: { email: user.email },
+  });
+  console.log('userFound=====>', userFound);
+});
+
 router.get('/users', async (req, res) => {
   const users = await prisma.user.findMany();
   res.json({ data: users });
